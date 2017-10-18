1d5b329406a35d6bd296893ec997d2011762a806
himhervert <halo_269@hotmail.com>
2017-10-18 09:00:56 -0600

    excepciones añadidas

--- src/EjemploExcepciones.java
+++ src/EjemploExcepciones.java
@@ -1,9 +1,31 @@
 /**
  * Created by Alumnos on 12/10/2017.
  */
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class EjemploExcepciones {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
+    {   try {
+        File archivo= new File("entrada.txt");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dame el primer numero:");
+        int num = teclado.nextInt();
+        System.out.println("Dame el 2o numero:");
+        int num2 = teclado.nextInt();
+        System.out.println("La division es :" + num / num2);
+    }
+    catch(ArithmeticException excepcionAritmetica)
     {
-        System.out.println("hola");
+        System.out.println("Division por 0");
+    }
+    catch(InputMismatchException mismatchexcepcion)
+    {
+        System.out.println("entrada incorrecta");
+    }
+    /*catch(FileNotFoundException e)
+    {
+        System.out.println("no existe el archivo");
+    }*/
     }
 }
